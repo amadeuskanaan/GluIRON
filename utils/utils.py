e4c2bde5f91048ccae27c90c0f5b27900e78c7ec
amadeuskanaan <amadeus.kanan@gmail.com>
2017-04-04 16:31:18 -0400

    push

--- utils/utils.py
+++ utils/utils.py
@@ -4,12 +4,15 @@ __author__ = 'kanaan'
 import os, errno
 
 def mkdir_path(path):
+    import os
+    import errno
     try:
         os.makedirs(path)
     except OSError as exc: # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else: raise
+    return path
 
 def find(name, path):
     import os
