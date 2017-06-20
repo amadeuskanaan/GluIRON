8a8a1d28e7097c866fad2385896172a774b28168
amadeuskanaan <amadeus.kanan@gmail.com>
2017-06-20 22:11:04 +0200

    push

--- randomize.py
+++ randomize.py
@@ -68,7 +68,7 @@ def run_randomise():
     con_file = os.path.join(stats_dir, 'design.con')
     mat_file = os.path.join(stats_dir, 'design.mat')
 
-    os.system('randomise -i %s -o randomise -d %s -t %s -R --uncorrp -n 50'
+    os.system('randomise -i %s -o randomise -d %s -t %s -T -D -R --uncorrp -n 5000'
               % (input_file, mat_file, con_file))
 
 prep_fsl_glm(df)
