a309bd3a75b967f08c20159193d84ad28003ff60
amadeuskanaan <amadeus.kanan@gmail.com>
2017-06-24 10:54:44 +0300

    push

--- 000_dcm_convert_lemon.py
+++ 000_dcm_convert_lemon.py
@@ -30,7 +30,7 @@ def get_niftis(population, afs_dir, workspace_dir):
 
         # input
         dicom_dir      = os.path.join(afs_dir, subject_id, 'MRI/DICOMS/uni/')
-        qsm_data_dir   = glob.glob(os.path.join(afs_dir, subject_id, 'MRI/*as_gre*'))
+        qsm_data_dir   = glob.glob(os.path.join(afs_dir, subject_id, 'MRI/*as_gre*'))[0]
         print qsm_data_dir
 
         # output
