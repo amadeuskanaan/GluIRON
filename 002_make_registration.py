50d8f2fcf6b9eebba2eabf50164a382042a0e3a0
amadeuskanaan <amadeus.kanan@gmail.com>
2017-07-01 19:36:50 +0200

    push

--- 002_make_registration.py
+++ 002_make_registration.py
@@ -134,41 +134,40 @@ def make_reg(population, workspace_dir, popname):
             os.system('flirt -in ../../QSM/QSM.nii -ref %s -applyxfm -init FLASH2MP2RAGE.mat -out QSM2MP2RAGE.nii.gz'%(unipp))
 
 
+        ################################################################################################################
 
-        # ################################################################################################################
-        #
-        #                                            # FLASH to MNI NON-LINEAR REG
-        #
-        # ################################################################################################################
-        # print '.....QSM to MNI'
-        #
-        # os.chdir(mni_dir)
-        #
-        # if not os.path.isfile('../QSM_MNI1mm.nii.gz'):
-        #     rsdir = '/scr/sambesi4/workspace/project_REST/study_%s' %workspace_dir[-1]
-        #     if os.path.isfile('%s/%s/ANATOMICAL/transform0Affine.mat'%(rsdir,subject)):
-        #         print 'using already run ants warps'
-        #         os.system('cp  %s/%s/ANATOMICAL/transform0Affine.mat ./MP2RAGE2MNI_affine.mat'%(rsdir,subject))
-        #         os.system('cp  %s/%s/ANATOMICAL/transform1Warp.nii.gz ./MP2RAGE2MNI_warp.nii.gz'%(rsdir,subject))
-        #         os.system('cp  %s/%s/ANATOMICAL/transform1InverseWarp.nii.gz ./MNI2MP2RAGE_warp.nii.gz'%(rsdir,subject))
-        #         os.system('flirt -in ../../QSM/QSM.nii -ref %s -applyxfm -init ../FLASH/FLASH2MP2RAGE.mat -out ../FLASH/QSM2MP2RAGE.nii.gz'%(unipp))
-        #         os.system('WarpImageMultiTransform 3 ../FLASH/QSM2MP2RAGE.nii.gz ../QSM_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (mni_brain_1mm))
-        #         os.system('WarpImageMultiTransform 3 ../FLASH/FLASH2MP2RAGE.nii.gz ../FLASH_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (mni_brain_1mm))
-        #     else:
-        #         print '..........Running ANTS'
-        #         run_ants(moving_img= unipp, ref_img= mni_brain_1mm, outpath= os.path.join(workspace_dir, subject, 'ANATOMICAL', 'MP2RAGE_UNI_PPROC_MNI1mm.nii.gz'))
-        #         os.system('mv  transform0Affine.mat MP2RAGE2MNI_affine.mat')
-        #         os.system('mv  transform1Warp.nii.gz MP2RAGE2MNI_warp.nii.gz')
-        #         os.system('mv  transform1InverseWarp.nii.gz MNI2MP2RAGE_warp.nii.gz')
-        #         os.system('WarpImageMultiTransform 3 %s ../MP2RAGE_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (unipp, mni_brain_1mm))
-        #         os.system('WarpImageMultiTransform 3 %s ../T1MAPS_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (t1map, mni_brain_1mm))
-        #         os.system('WarpImageMultiTransform 3 ../FLASH/QSM2MP2RAGE.nii.gz ../QSM_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (mni_brain_1mm))
-        #         os.system('WarpImageMultiTransform 3 ../FLASH/FLASH2MP2RAGE.nii.gz ../FLASH_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat'% (mni_brain_1mm))
-        #
-        # # os.system('WarpImageMultiTransform 3 %s ../T1MAPS_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (t1map, mni_brain_1mm))
-        # # os.system('WarpImageMultiTransform 3 %s ../MP2RAGE_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (unipp, mni_brain_1mm))
-        #
-        #
+                                                   # FLASH to MNI NON-LINEAR REG
+
+        ################################################################################################################
+        print '.....QSM to MNI'
+
+        os.chdir(mni_dir)
+
+        if not os.path.isfile('../QSM_MNI1mm.nii.gz'):
+            rsdir = '/scr/sambesi4/workspace/project_REST/study_%s' %workspace_dir[-1]
+            if os.path.isfile('%s/%s/ANATOMICAL/transform0Affine.mat'%(rsdir,subject)):
+                print 'using already run ants warps'
+                os.system('cp  %s/%s/ANATOMICAL/transform0Affine.mat ./MP2RAGE2MNI_affine.mat'%(rsdir,subject))
+                os.system('cp  %s/%s/ANATOMICAL/transform1Warp.nii.gz ./MP2RAGE2MNI_warp.nii.gz'%(rsdir,subject))
+                os.system('cp  %s/%s/ANATOMICAL/transform1InverseWarp.nii.gz ./MNI2MP2RAGE_warp.nii.gz'%(rsdir,subject))
+                os.system('flirt -in ../../QSM/QSM.nii -ref %s -applyxfm -init ../FLASH/FLASH2MP2RAGE.mat -out ../FLASH/QSM2MP2RAGE.nii.gz'%(unipp))
+                os.system('WarpImageMultiTransform 3 ../FLASH/QSM2MP2RAGE.nii.gz ../QSM_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (mni_brain_1mm))
+                os.system('WarpImageMultiTransform 3 ../FLASH/FLASH2MP2RAGE.nii.gz ../FLASH_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (mni_brain_1mm))
+            else:
+                print '..........Running ANTS'
+                run_ants(moving_img= unipp, ref_img= mni_brain_1mm, outpath= os.path.join(workspace_dir, subject, 'ANATOMICAL', 'MP2RAGE_UNI_PPROC_MNI1mm.nii.gz'))
+                os.system('mv  transform0Affine.mat MP2RAGE2MNI_affine.mat')
+                os.system('mv  transform1Warp.nii.gz MP2RAGE2MNI_warp.nii.gz')
+                os.system('mv  transform1InverseWarp.nii.gz MNI2MP2RAGE_warp.nii.gz')
+                os.system('WarpImageMultiTransform 3 %s ../MP2RAGE_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (unipp, mni_brain_1mm))
+                os.system('WarpImageMultiTransform 3 %s ../T1MAPS_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (t1map, mni_brain_1mm))
+                os.system('WarpImageMultiTransform 3 ../FLASH/QSM2MP2RAGE.nii.gz ../QSM_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (mni_brain_1mm))
+                os.system('WarpImageMultiTransform 3 ../FLASH/FLASH2MP2RAGE.nii.gz ../FLASH_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat'% (mni_brain_1mm))
+
+        # os.system('WarpImageMultiTransform 3 %s ../T1MAPS_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (t1map, mni_brain_1mm))
+        # os.system('WarpImageMultiTransform 3 %s ../MP2RAGE_MNI1mm.nii.gz -R %s MP2RAGE2MNI_warp.nii.gz MP2RAGE2MNI_affine.mat' % (unipp, mni_brain_1mm))
+
+
         # reg_dir_ = os.path.join(workspace_dir, subject, 'REGISTRATION')
         # os.chdir(reg_dir_)
         #
