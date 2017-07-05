a7591032d1aaaf4aa5e5acbf802cc26a0ba52646
amadeuskanaan <amadeus.kanan@gmail.com>
2017-07-05 10:41:17 +0200

    push

--- randomize.py
+++ randomize.py
@@ -1,6 +1,8 @@
+__author__ = 'kanaan'
 import os
 import pandas as pd
 from utils.utils import mkdir_path
+from variables_lemon import *
 
 datadir = '/scr/malta3/workspace/project_iron/'
 
@@ -18,31 +20,34 @@ df = pd.concat([df_controls, df_patients], axis =0)
 stats_dir = mkdir_path(os.path.join(datadir, 'statistics_norm'))
 os.chdir(stats_dir)
 
-__author__ = 'kanaan'
-
-
-def make_group_average(population, workspace):
+def make_group_average(population, workspace, popname):
 
     print 'creating group averages images '
-    stat_dir = os.path.join(workspace, 'GROUP_MEAN_IMAGE')
-    mkdir_path(stat_dir)
-    os.chdir(stat_dir)
+    average_dir = os.path.join(workspace, 'QSM_MEAN_MNI')
+    mkdir_path(average_dir)
+    os.chdir(average_dir)
 
-    qsm_list = [os.path.join(workspace, subject, 'REGISTRATION/QSM_MNI1mm.nii.gz') for subject in population]
-    os.system('fslmerge -t concat_qsm.nii.gz %s' % ' '.join(qsm_list))
-    os.system('fslmaths concat_qsm.nii.gz -Tmean QSM_mean.nii.gz')
+    for roi in ['QSM_norm_MNI1mm_STR.', 'QSM_norm_MNI1mm_R_STR', 'QSM_norm_MNI1mm_L_STR'
+                'QSM_norm_MNI1mm_BG',   'QSM_norm_MNI1mm'
+                ]:
 
-    t1_list = [os.path.join(workspace, subject, 'REGISTRATION/T1MAPS_MNI1mm.nii.gz') for subject in population ]
-    os.system('fslmerge -t concat_t1 %s' % ' '.join(t1_list))
-    os.system('fslmaths concat_t1 -Tmean T1MAPS_mean')
+        roi_name = roi[-10:]
+        qsm_list = [os.path.join(workspace, subject, 'QSM/%s.nii.gz'%roi) for subject in population]
+        os.system('fslmerge -t concat_qsm_%s %s' % (roi_name, ' '.join(qsm_list)))
+        os.system('fslmaths concat_qsm_%s -Tmean %s_QSM_mean_%s.nii.gz' %(roi_name, popname, roi_name))
 
-    uni_list = [os.path.join(workspace, subject, 'REGISTRATION/MP2RAGE_MNI1mm.nii.gz') for subject in population]
-    os.system('fslmerge -t concat_uni %s' % ' '.join(uni_list))
-    os.system('fslmaths concat_uni -Tmean UNI_mean')
+        # t1_list = [os.path.join(workspace, subject, 'REGISTRATION/T1MAPS_MNI1mm.nii.gz') for subject in population ]
+        # os.system('fslmerge -t concat_t1 %s' % ' '.join(t1_list))
+        # os.system('fslmaths concat_t1 -Tmean T1MAPS_mean')
 
-    os.system('rm -rf concat*')
+        # uni_list = [os.path.join(workspace, subject, 'REGISTRATION/MP2RAGE_MNI1mm.nii.gz') for subject in population]
+        # os.system('fslmerge -t concat_uni %s' % ' '.join(uni_list))
+        # os.system('fslmaths concat_uni -Tmean UNI_mean')
+        #
+        os.system('rm -rf concat*')
 
-#make_group_average(CONTROLS_QSM_A + PATIENTS_QSM_A, workspace_study_a)
+make_group_average(lemon_population, workspace_study_a)
+
 
 
 def prep_fsl_glm(df):
@@ -99,5 +104,6 @@ def run_randomise():
     # os.system('randomise -i %s -o randomise -d %s -t %s -R'
     #           % (input_file, mat_file, con_file))
 
-prep_fsl_glm(df)
-run_randomise()
+# prep_fsl_glm(df)
+# run_randomise()
+
