23799d832484859b92197d8f4030982ac1633d00
amadeuskanaan <amadeus.kanan@gmail.com>
2017-07-23 16:34:07 +0200

    push

--- 003_registration.py
+++ 003_registration.py
@@ -28,7 +28,7 @@ def make_reg(population, workspace_dir):
 
         # preprocessing Magnitude Image
         os.chdir(lin_dir)
-        if not os.path.isfile('FLASH_MAGNITUDE_BIAS_CORR_thr.nii'):
+        if not os.path.isfile('FLASH_MAGNITUDE_BIAS_CORR_thr.nii.gz'):
             os.system('N4BiasFieldCorrection -d 3 --input-image %s --output [FLASH_MAGNITUDE_BIAS_CORR.nii.gz, FLASH_MAGNITUDE_BIAS_FIELD.nii.gz ]'%mag)
             os.system('fslamaths FLASH_MAGNITUDE_BIAS_CORR -sub 0.02 -thr 0 -mul 8833.3 -min 255 FLASH_MAGNITUDE_BIAS_CORR_thr ')
 
