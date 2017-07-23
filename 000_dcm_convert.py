75b5603f40633e1fd79f7025576b5471478d6db6
amadeuskanaan <amadeus.kanan@gmail.com>
2017-07-23 14:11:54 +0200

    push

--- 000_dcm_convert.py
+++ 000_dcm_convert.py
@@ -25,36 +25,42 @@ def make_nifti(population, afs_dir, workspace_dir, pop_name):
         print 'Converting DICOM to nifti for Subject:', subject
 
         raw_dir   = mkdir_path(os.path.join(workspace_dir, subject, 'RAW'))
-        raw_anat  = mkdir_path(os.path.join(raw_dir, 'uni'))
+        raw_uni  = mkdir_path(os.path.join(raw_dir, 'uni'))
         raw_qsm   = mkdir_path(os.path.join(raw_dir, 'qsm'))
         anat_dir  = mkdir_path(os.path.join(workspace_dir, subject, 'ANATOMICAL'))
         qsm_dir   = mkdir_path(os.path.join(workspace_dir, subject, 'QSM'))
 
-        # Copy mp2rage_uni data
-        if pop_name ==  'GTS':
-            dicoms = [os.path.join(dicom_dir, dicom) for dicom in os.listdir(dicom_dir)]
-            uni_imgs = [dicom for dicom in dicoms if'UNI_Images' in pydicom.read_file(dicom, force=True).SeriesDescription]
 
-        elif pop_name == 'LEMON':
-            uni_imgs = [os.path.join(dicom_dir, dicom) for dicom in os.listdir(dicom_dir)]
+        ##############################################
+        #  Copy mp2rage_uni data
+
+        if not os.path.isfile(os.path.join(anat_dir, 'UNI.nii')):
+
+            if pop_name ==  'GTS':
+                dicoms = [os.path.join(dicom_dir, dicom) for dicom in os.listdir(dicom_dir)]
+                uni_imgs = []
+                for dicom in dicoms:
+                    try:
+                        SeriesDescription = pydicom.read_file(dicom, force=True).SeriesDescription
+                    except AttributeError:
+                        continue
+                    if 'mp2rage' in SeriesDescription:
+                        mp2rage_list.append(dicom)
+
+            elif pop_name == 'LEMON':
+                uni_imgs = [os.path.join(dicom_dir, dicom) for dicom in os.listdir(dicom_dir)]
+
+            for uni in uni_imgs:
+                shutil.copy(uni, raw_anat)
+
+            os.system('isisconv -in %s -out %s/UNI.nii -rf dcm -wdialect fsl' %(raw_uni, anat_dir, subject))
 
 
-        print uni_imgs
+        ##############################################
 
 
-        # # Grab MP2RAGE DATA
-        # if pop_name == 'GTS':
-        #
-        #     dicoms   = [os.path.join(dicom_dir, dicom) for dicom in os.listdir(dicom_dir)]
-        #     uni_imgs = [dicom for dicom in dicoms if 'UNI_Images' in pydicom.read_file(dicom, force=True).SeriesDescription ]
-        #
-        # elif pop_name == 'LEMON':
-        #     dicom_dir = os.path.join(afs_dir, subject_id, 'MRI/DICOMS/uni/')
 
-
-        # Grab QSM multi-channel data
-
-make_nifti(['BATP'], afs_patients, workspace_study_a, 'GTS')
+# make_nifti(['BATP'], afs_patients, workspace_study_a, 'GTS')
 make_nifti(['LEMON891/LEMON113'], afs_lemon, workspace_study_a, 'LEMON')
 
 
