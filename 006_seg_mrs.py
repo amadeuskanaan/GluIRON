c23a4426ba77df930760148436b95a14acfef9ee
amadeuskanaan <amadeus.kanan@gmail.com>
2017-07-29 21:14:43 +0200

    push

--- 006_seg_mrs.py
+++ 006_seg_mrs.py
@@ -91,5 +91,5 @@ def get_mrs_masks(population, afs, workspace_dir):
         create_svs_mask('ACC', ['ACC', 'acc', 'Acc'])
 
 
-get_mrs_masks(['AA8P'], afs_controls, workspace_iron) # WSKT
-# get_mrs_masks(patients_a, afs_patients, workspace_iron)
+get_mrs_masks(controls_a, afs_controls, workspace_iron) # WSKT
+get_mrs_masks(patients_a, afs_patients, workspace_iron)
