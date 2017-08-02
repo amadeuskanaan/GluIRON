ff67c4bd686cc4538493b6034c237bbdad627816
amadeuskanaan <amadeus.kanaan@gmail.com>
2017-08-02 13:52:24 +0200

    000_dcm_convert.py

--- 007_quality_control.py
+++ 007_quality_control.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-from utils.utils import  mkdir_path, calculate_framewise_displacement_fsl, plot_nucleus, plot_qsm_multi, plot_qsm_single
+from utils.utils import  mkdir_path#, calculate_framewise_displacement_fsl, plot_nucleus, plot_qsm_multi, plot_qsm_single
 from utils.spatial_qc import *
 from variables import *
 from matplotlib import colors
@@ -31,14 +31,14 @@ def extract_quality_metrics(population, workspace_dir):
         subject_dir = os.path.join(workspace_dir, subject)
         qc_dir      = mkdir_path(os.path.join(subject_dir, 'QUALITY_CONTROL'))
 
-        mp2rage_uni = os.path.join(subject_dir, 'ANATOMICAL', 'MP2RAGE_UNI.nii.gz')
-        mp2rage_mas = os.path.join(subject_dir, 'ANATOMICAL', 'BRAIN_MASK.nii.gz')
-        mp2rage_gm  = os.path.join(subject_dir, 'ANATOMICAL', 'seg/c1MP2RAGE_UNI.nii')
-        mp2rage_wm  = os.path.join(subject_dir, 'ANATOMICAL', 'seg/c2MP2RAGE_UNI.nii')
-        mp2rage_cm  = os.path.join(subject_dir, 'ANATOMICAL', 'seg/c3MP2RAGE_UNI.nii')
+        # mp2rage_uni = os.path.join(subject_dir, 'ANATOMICAL', 'MP2RAGE_UNI.nii.gz')
+        # mp2rage_mas = os.path.join(subject_dir, 'ANATOMICAL', 'BRAIN_MASK.nii.gz')
+        # mp2rage_gm  = os.path.join(subject_dir, 'ANATOMICAL', 'seg/c1MP2RAGE_UNI.nii')
+        # mp2rage_wm  = os.path.join(subject_dir, 'ANATOMICAL', 'seg/c2MP2RAGE_UNI.nii')
+        # mp2rage_cm  = os.path.join(subject_dir, 'ANATOMICAL', 'seg/c3MP2RAGE_UNI.nii')
 
-        flash_mag = os.path.join(subject_dir, 'QSM/FLASH_PHASE.nii')
-        flash_mas = os.path.join(subject_dir, 'QSM/mask.nii.gz')
+        flash_mag = os.path.join(subject_dir, 'QSM/FLASH_MAGNITUDE.nii')
+        flash_mas = os.path.join(subject_dir, 'QSM/brain_mask.nii.gz')
         flash_gm  = os.path.join(subject_dir, 'REGISTRATION/FLASH_GM_opt.nii.gz')
         flash_wm  = os.path.join(subject_dir, 'REGISTRATION/FLASH_WM_opt.nii.gz')
         flash_cm  = os.path.join(subject_dir, 'REGISTRATION/FLASH_CSF_opt.nii.gz')
@@ -65,7 +65,7 @@ def extract_quality_metrics(population, workspace_dir):
             qc_fwhm = fwhm(img, mask, out_vox=False)
             qc_efc  = efc(img_data)
             qc_fber = fber(img_data, mas_data)
-            qc_qi1 = artifacts(img_data, mas_data, 'UNI')
+            qc_qi1 = artifacts(img_data, mas_data, 'MAG')
 
             return qc_cnr, qc_snr, qc_fwhm[3], qc_efc, qc_fber, qc_qi1
 
@@ -73,18 +73,18 @@ def extract_quality_metrics(population, workspace_dir):
 
             #uni_qc_cnr, uni_qc_snr, uni_qc_fwhm, uni_qc_efc, uni_qc_fber, uni_qc_qi1 = calc_spatial_metrics(mp2rage_uni, mp2rage_mas, mp2rage_gm, mp2rage_wm, mp2rage_cm)
             mag_qc_cnr, mag_qc_snr, mag_qc_fwhm, mag_qc_efc, mag_qc_fber, mag_qc_qi1 = calc_spatial_metrics(flash_mag, flash_mas, flash_gm, flash_wm, flash_cm)
-
-            cols = [#'SNR_UNI', 'CNR_UNI', 'FBER_UNI', 'EFC_UNI', 'FWHM_UNI', 'QI1_UNI',
-                    'SNR_MAG', 'CNR_MAG', 'FBER_MAG', 'EFC_MAG', 'FWHM_MAG', 'QI1_MAG',
-                    ]
-            df = pd.DataFrame(columns=cols, index=['%s' % subject])
-
-            #df.loc[subject]['SNR_UNI']  = uni_qc_snr
-            #df.loc[subject]['CNR_UNI']  = uni_qc_cnr
-            #df.loc[subject]['FBER_UNI'] = uni_qc_fber
-            #df.loc[subject]['EFC_UNI']  = uni_qc_efc
-            #df.loc[subject]['FWHM_UNI'] = uni_qc_fwhm
-            #df.loc[subject]['QI1_UNI']  = uni_qc_qi1
+            #
+            qc_metrics = [#'SNR_UNI', 'CNR_UNI', 'FBER_UNI', 'EFC_UNI', 'FWHM_UNI', 'QI1_UNI',
+                           'SNR_MAG', 'CNR_MAG', 'FBER_MAG', 'EFC_MAG', 'FWHM_MAG', 'QI1_MAG'
+                         ]
+            df = pd.DataFrame(columns=qc_metrics, index=['%s' % subject])
+            #
+            # #df.loc[subject]['SNR_UNI']  = uni_qc_snr
+            # #df.loc[subject]['CNR_UNI']  = uni_qc_cnr
+            # #df.loc[subject]['FBER_UNI'] = uni_qc_fber
+            # #df.loc[subject]['EFC_UNI']  = uni_qc_efc
+            # #df.loc[subject]['FWHM_UNI'] = uni_qc_fwhm
+            # #df.loc[subject]['QI1_UNI']  = uni_qc_qi1
             df.loc[subject]['SNR_MAG']  = mag_qc_snr
             df.loc[subject]['CNR_MAG']  = mag_qc_cnr
             df.loc[subject]['FBER_MAG'] = mag_qc_fber
@@ -92,19 +92,12 @@ def extract_quality_metrics(population, workspace_dir):
             df.loc[subject]['FWHM_MAG'] = mag_qc_fwhm
             df.loc[subject]['QI1_MAG']  = mag_qc_qi1
 
-            pca = TruncatedSVD(n_components=1)
-            qc_metrics = ['EFC_MAG', 'FWHM_MAG', 'QI1_MAG']  # 'SNR_MAG', 'CNR_MAG', 'FBER_MAG',
-            pca.fit(np.array(np.asarray([df_qsm[qc] for qc in qc_metrics])))
-            df_qsm['QC_PCA_lower'] = pca.components_[0, :]
-
-            pca = TruncatedSVD(n_components=1)
-            qc_metrics = ['SNR_MAG', 'CNR_MAG', 'FBER_MAG']  # 'SNR_MAG', 'CNR_MAG', 'FBER_MAG',
-            pca.fit(np.array(np.asarray([df_qsm[qc] for qc in qc_metrics])))
-            df_qsm['QC_PCA_higher'] = pca.components_[0, :]
-
             df.to_csv(os.path.join(qc_dir, 'QC.csv'))
 
 
-extract_quality_metrics(patients_a, workspace_iron)
-extract_quality_metrics(lemon_population, workspace_iron)
-extract_quality_metrics(controls_a, workspace_iron)
+# extract_quality_metrics(controls_a, workspace_iron)
+# extract_quality_metrics(patients_a, workspace_iron)
+# extract_quality_metrics(lemon_population[0:25], workspace_iron)
+# extract_quality_metrics(lemon_population[25:50], workspace_iron)
+# extract_quality_metrics(lemon_population[50:75], workspace_iron)
+extract_quality_metrics(lemon_population[75:], workspace_iron)
