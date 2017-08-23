f1ee24d2817566fbd109dcf981cdbdcce28f7d96
amadeuskanaan <amadeus.kanan@gmail.com>
2017-08-23 20:57:50 +0200

    push

--- 010_randomize.py
+++ 010_randomize.py
@@ -183,7 +183,7 @@ def randomize_one_sample(df):
     for roi in rois:
         print '######################################'
         print 'Running randomise for roi:', roi
-        qsm_list = [os.path.join(workspace, subject, 'QSM/QSMnorm_MNI1mm_%s.nii.gz' % roi) for subject in
+        qsm_list = [os.path.join(workspace_iron, subject, 'QSM/QSMnorm_MNI1mm_%s.nii.gz' % roi) for subject in
                     population]
         # print qsm_list
         stats_dir = mkdir_path(os.path.join(ahba_dir, 'RANDOMISE'))
