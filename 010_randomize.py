c83693b2b96ff4e49e70ac6d6a949d90171a6357
amadeuskanaan <amadeus.kanan@gmail.com>
2017-08-27 11:46:56 +0200

    push

--- 010_randomize.py
+++ 010_randomize.py
@@ -78,7 +78,9 @@ def make_nuclei_group_average(population,workspace, popname):
 
 def randomize_two_sample(df):
 
-    stats_dir = mkdir_path(os.path.join(ahba_dir, 'RANDOMISE3'))
+    permutation = '0'
+
+    stats_dir = mkdir_path(os.path.join(ahba_dir, 'RANDOMISE_%s'%permutation))
     os.chdir(stats_dir)
 
     population = df.index
@@ -133,11 +135,11 @@ def randomize_two_sample(df):
             print 'Running randomise for roi:', roi
             qsm_list = [os.path.join(workspace_iron, subject, 'QSM/QSMnorm_MNI1mm_%s.nii.gz' % roi) for subject in population]
             #print qsm_list
-            stats_dir = mkdir_path(os.path.join(ahba_dir,  'RANDOMISE3'))
+            stats_dir = mkdir_path(os.path.join(ahba_dir, 'RANDOMISE_%s'%permutation))
             os.chdir(stats_dir)
             os.system('fslmerge -t concat_CP_%s.nii.gz %s' % (roi, ' '.join(qsm_list)))
             os.system('randomise -i concat_CP_%s -o randomise_CP_%s -d design_twosample.mat -t design_twosample.con -R --uncorrp '
-                      '-T -n 20000 -x'
+                      #'-T -n 20000 -x'
                       % (roi, roi))
             os.system('rm -rf *concat*')
 
