98230be15b77d2854fa5756c116df768634b56f3
amadeuskanaan <amadeus.kanaan@gmail.com>
2017-09-06 17:32:49 +0200

    push

--- 011_ahba_api.py
+++ 011_ahba_api.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 from alleninf.api import get_probes_from_genes
 from alleninf.data import get_values_at_locations
 from alleninf.api import get_mni_coordinates_from_wells#
@@ -14,6 +15,7 @@ import statsmodels.formula.api as smf
 import math
 from variables.variables import *
 from AHBA.genesets import *
+from sklearn.decomposition import TruncatedSVD, PCA
 api_url        = "http://api.brain-map.org/api/v2/data/query.json"
 
 
@@ -58,21 +60,30 @@ def return_probe_expression(gene_probes_dict, geneset_name):
         all_probes = ['%s_' % gene + str(i) for gene in gene_probes_dict.keys() for i in
                       gene_probes_dict[gene].values()]
 
-        df['Mean'] = df[all_probes].mean(axis=1)
-        df['Median'] = df[all_probes].median(axis=1)
 
-        from sklearn.decomposition import TruncatedSVD, PCA
-        pca = PCA(n_components=5)
-        pca.fit(np.array(np.asarray([df[probe] for probe in all_probes])))
-        df['PC1'] = pca.components_[0, :]
-        if  pca.components_[1, :].any():
-            df['PC2'] = pca.components_[1, :]
-        if pca.components_[2, :].any():
-            df['PC3'] = pca.components_[2, :]
-        print 'PC explained variance:', pca.explained_variance_ratio_
-        #df['PC_EV'] = pca.explained_variance_ratio_[0]#, pca.explained_variance_ratio_[1], pca.explained_variance_ratio_[2],
+        # Need to simplify Dataframe by averaging all probes for every gene
+        #split dataframe in probes and metadata
+        df_probes     = df.iloc[:, :-5]
+        df_metadata   = df.iloc[:, -5:]
+        probes_unique = [s.split('_')[0] for s in df_probes.T.index.values]
+        df_probes_mu  = df_probes.astype(float).T.groupby(probes_unique).mean().T
 
-        #package_directory = '/Users/kanaan/SCR/Github/alleninf/alleninf'
+        df = pd.concat([df_probes_mu, df_metadata], axis=1)
+        if len(set(probes_unique)) > 1:
+            df['Mean'] = df[list(set(probes_unique))].mean(axis=1)
+            df['Median'] = df[list(set(probes_unique))].median(axis=1)
+            pca = PCA(n_components=3)
+            pca.fit(np.array(np.asarray([df[gene] for gene in genes])))
+            df['PC1'] = pca.components_[0, :]
+            if  pca.components_[1, :].any():
+                df['PC2'] = pca.components_[1, :]
+            if len(genes) > 3:
+                df['PC3'] = pca.components_[2, :]
+            print 'PC explained variance:', pca.explained_variance_ratio_
+            #df['PC_EV'] = pca.explained_variance_ratio_[0]#, pca.explained_variance_ratio_[1], pca.explained_variance_ratio_[2],
+
+
+            #package_directory = '/Users/kanaan/SCR/Github/alleninf/alleninf'
         package_directory = '/scr/malta1/Software/anaconda/envs/awesome/lib/python2.7/site-packages/alleninf'
         mni = pd.read_csv(os.path.join(package_directory, "data", "corrected_mni_coordinates.csv"),
                           header=0, index_col=0)
@@ -80,7 +91,7 @@ def return_probe_expression(gene_probes_dict, geneset_name):
         df_concat = pd.concat([df, mni], axis=1).to_csv(os.path.join(ahba_dir, 'AHBA_%s.csv' % geneset_name))
 
         return df_concat
-    #return pd.read_csv(os.path.join(ahba_dir, 'AHBA_%s.csv' % geneset_name), index_col=0)
+    return pd.read_csv(os.path.join(ahba_dir, 'AHBA_%s.csv' % geneset_name), index_col=0)
 
 def get_expression_df(genes, geneset_name):
     gene_probes = {}
@@ -90,7 +101,11 @@ def get_expression_df(genes, geneset_name):
     return_probe_expression(gene_probes, geneset_name)
     #return df
 
-get_expression_df(FTH                   , 'FTH')
+
+genesets = ['IRON', 'IRON_D', 'DA_jellen', 'DA_jellen2', 'DA_metabolism', 'DA_receptor', 'DA_receptor_sig', 'DA_tranmission', 'DA_transport',
+            'ANMC', 'GLU', 'GABA', 'FTH', 'TF','FTH_ALL', 'FTL_ALL', 'FERRITIN']
+
+
 get_expression_df(IRON.keys()           , 'IRON')
 get_expression_df(IRON_D.keys()         , 'IRON_D')
 get_expression_df(DA_jellen             , 'DA_jellen')
@@ -105,11 +120,14 @@ get_expression_df(GLU_metabolism        , 'GLU')
 get_expression_df(GABA_metabolism       , 'GABA')
 get_expression_df(GLU_GABA              , 'GLU_GABA')
 get_expression_df(TF                    , 'TF')
+get_expression_df(FTH                   , 'FTH')
 get_expression_df(FTL                   , 'FTL')
 get_expression_df(HRE                   , 'HRE')
 get_expression_df(HRE2                  , 'HRE2')
 get_expression_df(SLC25                 , 'SLC25')
-# get_expression_df(SLC40                 , 'SLC40')
 get_expression_df(BIOBANK               , 'BIOBANK')
 get_expression_df(HOUSEKEEPING          , 'HOUSEKEEPING')
+get_expression_df(FTH_ALL               , 'FTH_ALL')
+get_expression_df(FTL_ALL               , 'FTL_ALL')
+get_expression_df(FERRITIN              , 'FERRITIN')
 
