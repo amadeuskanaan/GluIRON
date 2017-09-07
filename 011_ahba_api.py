c92b2195e63122e00c2bb790e056f71315def413
amadeuskanaan <amadeus.kanan@gmail.com>
2017-09-07 17:25:45 +0200

    push

--- 011_ahba_api.py
+++ 011_ahba_api.py
@@ -131,6 +131,7 @@ genesets = ['IRON', 'IRON_D', 'DA_jellen', 'DA_jellen2', 'DA_metabolism', 'DA_re
 # get_expression_df(FTL_ALL               , 'FTL_ALL')
 # get_expression_df(FERRITIN              , 'FERRITIN')
 # get_expression_df(AHBA_GENELIST         , 'GENELIST')
-# get_expression_df(AHBA_GENELIST_FRENCH    , 'GENELIST_FRENCH')
-for gene in AHBA_GENELIST_FRENCH:
-    print gene
+
+print len(AHBA_GENELIST_FRENCH)
+get_expression_df(AHBA_GENELIST_FRENCH    , 'GENELIST_FRENCH')
+
