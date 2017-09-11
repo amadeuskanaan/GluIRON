ba3a9d83c5e307d2b0943da2a57b9d769ba5045f
amadeuskanaan <amadeus.kanan@gmail.com>
2017-09-11 10:32:15 +0200

    push

--- 010_randomize.py
+++ 010_randomize.py
@@ -139,8 +139,8 @@ def randomize_two_sample(df):
            #'L_Caud', 'L_Puta', 'L_Pall',
            #'R_Caud', 'R_Puta', 'R_Pall',
            #'STR3_MOTOR', 'STR3_EXEC', 'STR3_LIMBIC'
-           'Caud',
-           #'Puta',
+           # 'Caud',
+           'Puta',
            # 'Pall',
            #'STR',
            #'L_STR', 'R_STR',
