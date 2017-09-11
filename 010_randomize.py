8959ca90a9464d33a8602e22a0b851f60dc6318c
amadeuskanaan <amadeus.kanan@gmail.com>
2017-09-11 09:44:59 +0200

    push

--- 010_randomize.py
+++ 010_randomize.py
@@ -22,7 +22,10 @@ qc_outliers_c  = []
 qc_outliers_p  = ['NL2P', 'HSPP', 'STDP', 'DF2P'] # 'LA9P'
 
 
-rois = ['L_Caud', 'L_Puta', 'R_Caud', 'R_Puta', 'L_Pall', 'R_Pall']
+rois = ['L_Caud', 'L_Puta', 'R_Caud', 'R_Puta', 'L_Pall', 'R_Pall'
+        'Caud', 'Puta', 'Pall',
+        'L_STR', 'R_STR', 'STR'
+        ]
 
 def get_dfs():
     dfc = pd.read_csv(os.path.join(phenotypic_dir, 'df_raw_controls.csv'), index_col = 0).drop(qc_outliers_c, axis = 0)
