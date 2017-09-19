660be86d7b7faf1e34edeb81002886dd9fbead61
amadeuskanaan <amadeus.kanan@gmail.com>
2017-09-19 16:32:20 +0200

    push

--- 007_transform_nuclei.py
+++ 007_transform_nuclei.py
@@ -59,7 +59,7 @@ def transform_nuclei(population, workspace):
                               '-t %s %s' % (roi, roi, mni_brain_1mm, uni2mni_w, uni2mni_a))
                     os.system('fslmaths %s2MNI -thr 0.5 -bin -mul %s QSMnorm_MNI1mm_%s' % (roi, qsm, roi))
                     os.system('rm -rf %s2MP2RAGE* %s2MNI*' % (roi, roi))
-                elif roi == 'GM':
+                if not os.path.isfile('QSMnorm_MNI1mm_%s_05.nii.gz' % roi):
                     nuc = os.path.join(subject_dir, 'REGISTRATION/FLASH_GM_opt')
                     os.system('flirt -in %s -ref %s -applyxfm -init %s -out %s2MP2RAGE' % (nuc, uni, qsm2uni, roi))
                     os.system('antsApplyTransforms -d 3 -i %s2MP2RAGE.nii.gz -o %s2MNI.nii.gz -r %s -n Linear '
